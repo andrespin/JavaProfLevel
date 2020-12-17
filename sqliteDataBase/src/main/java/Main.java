1bfb35b3973bd0dc27e0483b6bc22ad960320c48
Andrew <andrespin@mail.ru>
2020-12-17 14:22:50 +0300

    Изменения

--- sqliteDataBase/src/main/java/Main.java
+++ sqliteDataBase/src/main/java/Main.java
@@ -16,10 +16,26 @@ import java.sql.*;
 
 public class Main {
 
-
+    private static DataBaseManager dataBaseManager = new DataBaseManager();
 
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        goodsDataBaseProcessing();
+    }
 
+    // Подскажите как исправить данные баги
+    private static void goodsDataBaseProcessing() throws SQLException, ClassNotFoundException {
+        dataBaseManager.connection();
+        // Вот тут при создании таблицы создаётся колонка "good_idINTEGER" вместо "good_id" (аналогично и с другими),
+        // в результате в SQL-запросе далее приходится прописывать good_namevarchar, иначе ошибка - no such colomn
+        dataBaseManager.createTable("goods","good_id", "good_name", "good_price");
+        dataBaseManager.clearTable("goods");
+        // Тут при заполнении таблицы не получилось заполнить good_id, постоянно вылетает ошибка при попытке это сделать
+        dataBaseManager.insertTable("goods", 10000);
+        dataBaseManager.findProductPrice("goods", "productName2");
+        dataBaseManager.replaceProductPrice("goods", "productName2", "1000");
+        // а тут как бы ни писал название таблицы (то ли good_namevarchar, то ли good_name) всегда вылетает ошибка:
+        // [SQLITE_ERROR] SQL error or missing database (no such column: good_namevarchar)
+        dataBaseManager.findGoodsInPriceRange("goods" ,1200, 1800);
     }
 
 }
